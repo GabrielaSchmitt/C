a84030d39b76cb5dfc91d88602178c92ca778471
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2022-10-13 13:50:05 -0300

    Rename program1.cpp to program1.c

--- program1.c
+++ program1.c
@@ -1,4 +1,4 @@
-//FaÁa um programa que leia os lados de um tri‚ngulo e informe qual o seu tipo: equil·tero, isÛceles ou escaleno. Os lados devem ser n˙meros inteiros, sem sinal.
+//Fa√ßa um programa que leia os lados de um tri√¢ngulo e informe qual o seu tipo: equil√°tero, is√≥celes ou escaleno. Os lados devem ser n√∫meros inteiros, sem sinal.
 
 #include <stdio.h>
 #include <locale.h>
@@ -6,9 +6,9 @@
 int main() {
 
 setlocale(LC_ALL, "Portuguese");
-    printf("--Analisador de Tri‚ngulos--\n ");
+    printf("--Analisador de Tri√¢ngulos--\n ");
     
-    // declare as vari·veis que ser„o utilizadas
+    // declare as vari√°veis que ser√£o utilizadas
     float r1, r2, r3;
     
     //recebendo valores
@@ -21,22 +21,22 @@ setlocale(LC_ALL, "Portuguese");
     printf("Informe o Terceiro seguimento: ");
     scanf("%f", &r3);
 
-   //LÛgica do programa
+   //L√≥gica do programa
     if (r1 < r2 + r3 && r2 < r1 + r3 && r3 < r1 + r2)
     {
-        printf("Os seguimentos informados podem formar um tri‚ngulo, ");
+        printf("Os seguimentos informados podem formar um tri√¢ngulo, ");
     if (r1 == r2  && r1 == r3){
-            printf("Este È um tri‚ngulo Equil·tero.");
+            printf("Este √© um tri√¢ngulo Equil√°tero.");
     }
     else if (r1 != r2 && r2 != r3 && r1 != r3) {
-            printf("Este tri‚ngulo È Escaleno.");
+            printf("Este tri√¢ngulo √© Escaleno.");
     }
     else if ((r1 == r2 && r1 != r3) || (r1 == r3 && r1 != r2) || (r2 == r3 && r2 != r1)){
-        printf("Este tri‚ngulo È IsÛsceles.");
+        printf("Este tri√¢ngulo √© Is√≥sceles.");
     }
     }
     else{ 
-        printf("Os seguimentos informados N√O podem formar um tri‚ngulo.");
+        printf("Os seguimentos informados N√ÉO podem formar um tri√¢ngulo.");
     }
     return 0;
 }
