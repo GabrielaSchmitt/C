2a96c6b6baf9a7286915f61bdbad88501fb5dfeb
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2022-10-13 13:50:23 -0300

    Rename program10.cpp to program10.c

--- program10.c
+++ program10.c
@@ -1,14 +1,14 @@
-/* Faça um jogo de loteria! O programa sorteia 6 números (chamadas dezenas) de 1 a 60 e pede
-para o jogador escolher 6 números de 1 a 60. O programa compara os números escolhidos com os 
-números sorteados, mostra as dezenas sorteadas, as dezenas escolhidas pelo jogador e mostra o 
-prêmio conforme abaixo:
+/* FaÃ§a um jogo de loteria! O programa sorteia 6 nÃºmeros (chamadas dezenas) de 1 a 60 e pede
+para o jogador escolher 6 nÃºmeros de 1 a 60. O programa compara os nÃºmeros escolhidos com os 
+nÃºmeros sorteados, mostra as dezenas sorteadas, as dezenas escolhidas pelo jogador e mostra o 
+prÃªmio conforme abaixo:
 	6 acertos: R$ 1.000.000,00
 	5 acertos: R$ 250.000,00
 	4 acertos: R$ 50.000,00
 	3 acertos: nada.
 	2 acertos: nada.
 	1 acerto: nada.
-Obs.: validar os números digitados pelo jogador, devem ser entre 1 e 60. */
+Obs.: validar os nÃºmeros digitados pelo jogador, devem ser entre 1 e 60. */
 
 #include <stdio.h>
 #include <conio.h>
@@ -21,13 +21,13 @@ int main() {
 	setlocale(LC_ALL, "Portuguese");
 	srand(time(NULL));
 	
-	// declarações
+	// declaraÃ§Ãµes
 	int loteria[6], i, teste, v, j, k, acerto;
-	int aposta[6];  // números apostados
+	int aposta[6];  // nÃºmeros apostados
 	
 	// recebendo valores
 	
-	for (i = 0; i < 6; i++){     // sorteia 6 números de 1 - 60 que não se repetem
+	for (i = 0; i < 6; i++){     // sorteia 6 nÃºmeros de 1 - 60 que nÃ£o se repetem
 		if(i == 0){                        
 			loteria[i] = 1 + rand() %60;     
 		}else {                            
@@ -48,19 +48,19 @@ int main() {
 	for(k =0; k < 6; k++){
 	teste = 0;
 		while(teste == 0){
-		printf("Digite o %d° número de 1 à 60 para apostar na loteria: ", k+1);
+		printf("Digite o %dÂ° nÃºmero de 1 Ã  60 para apostar na loteria: ", k+1);
 		scanf(" %d", &aposta[k]);
 			if (aposta[k] > 0 && aposta [k] < 61){
 				teste = 1;
 				break;
 			}else {
-				printf("O número deve estar entre 1 e 60!\n");
+				printf("O nÃºmero deve estar entre 1 e 60!\n");
 				teste = 0;
 			}
 		}
 	}
 	
-	// comparação:
+	// comparaÃ§Ã£o:
 	acerto = 0;
 	
 	for (int m = 0; m < 6; m++){
@@ -84,7 +84,7 @@ int main() {
 
 	if(acerto == 1 || acerto == 2 || acerto == 3){
 		printf("\nAcertos: %d", acerto);
-		printf("\nPrêmio: nada.");
+		printf("\nPrÃªmio: nada.");
 	} else if (acerto == 4){
 		printf("\n4 acertos: R$ 50.000,00");
 	} else if (acerto == 5){
@@ -92,7 +92,7 @@ int main() {
 	} else if (acerto == 6){
 		printf("\n6 acertos: R$ 1.000.000,00");
 	} else {
-		printf("\nBoa sorte na próxima vez!");	
+		printf("\nBoa sorte na prÃ³xima vez!");	
 	}
 	
 	return 0;
