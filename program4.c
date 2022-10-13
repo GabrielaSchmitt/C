cd51a669cdd36c94962cb1818c1c4e9b28374122
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2022-10-13 13:51:33 -0300

    Rename program4.cpp to program4.c

--- program4.c
+++ program4.c
@@ -1,4 +1,4 @@
-// Faça um programa que leia os coeficientes a, b e c de uma equação de segundo grau, calcule o valor do discriminante delta D e os valores das raízes quadradas X1 e X2. No término, o programa deve mostrar as raízes para o usuário.
+// FaÃ§a um programa que leia os coeficientes a, b e c de uma equaÃ§Ã£o de segundo grau, calcule o valor do discriminante delta D e os valores das raÃ­zes quadradas X1 e X2. No tÃ©rmino, o programa deve mostrar as raÃ­zes para o usuÃ¡rio.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,7 @@
 int main() {
 	
 	setlocale(LC_ALL, "Portuguese");
-    // declarações
+    // declaraÃ§Ãµes
     float a, b, c, d, x1, x2;
     
     //recebendo valores
@@ -21,11 +21,11 @@ int main() {
     printf("Informe o valor de C: ");
     scanf("%f", &c);
     
-    //lógica do programa
+    //lÃ³gica do programa
     d =  (b*b) -4 * a * c;
     
     if (d < 0 ){
-    	printf("A equação não tem raízes reais pois d é menor que zero. ");
+    	printf("A equaÃ§Ã£o nÃ£o tem raÃ­zes reais pois d Ã© menor que zero. ");
 	}else{
 		x1 = (-b - sqrtf(d)) / (2 * a);
 		x2 = (-b + sqrtf(d)) / (2 * a);
